1fb6636235bc4aa44a0c18d0d39505bc0728edb4
masdimaseka <dimasekaid@gmail.com>
2024-10-23 16:27:08 +0800

    home, login, and navbar

---
+++ components/navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg bg-white border-bottom border-1">
+    <div class="container-fluid container">
+        <a class="navbar-brand text-primary fw-bold" href="index.php">Tobaku Olifia</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container-fluid collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav gap-3">
+                <li class="nav-item">
+                    <a class="nav-link text-primary" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="input-data-buku.php">Customer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="input-data-pengarang.php">Barang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="input-data-penerbit.php">Transaksi</a>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-primary">
+                        <i class="bi bi-person-fill"></i>
+                        Username
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-secondary">
+                        <i class="bi bi-person-fill"></i>
+                        Log Out
+                    </button>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
