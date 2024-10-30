33aa857ab6abbe42b71d05bfd1b713bdcff01a98
masdimaseka <dimasekaid@gmail.com>
2024-10-31 00:45:30 +0800

    finish input data customer & barang

--- input-data-barang.php
+++ input-data-barang.php
@@ -14,7 +14,39 @@
     include_once("components/navbar.php")
     ?>
     <div class="container">
-        <h1>input data barang</h1>
+        <div class="container-form mt-4 mb-5 px-5 py-5 rounded-3">
+            <h1>Input Data Barang</h1>
+            <form action="">
+                <div class="mt-5">
+                    <label for="kodeBarang" class="form-label">Kode Barang</label>
+                    <input type="text" class="form-control" id="kodeBarang" placeholder="12345" disabled>
+                </div>
+                <div class="mt-4">
+                    <label for="namaBarang" class="form-label">Nama Barang</label>
+                    <input type="text" class="form-control" id="namaBarang" placeholder="nama barang...">
+                </div>
+                <div class="mt-4">
+                    <label for="satuanBarang" class="form-label">Satuan</label>
+                    <input type="text" class="form-control" id="satuanBarang" placeholder="satuan barang...">
+                </div>
+                <div class="mt-4">
+                    <label for="stokAwalBarang" class="form-label">Stok Awal</label>
+                    <input type="number" class="form-control" id="stokAwalBarang" placeholder="sok awal barang...">
+                </div>
+                <div class="mt-4">
+                    <label for="hppBarang" class="form-label">HPP Barang</label>
+                    <input type="number" class="form-control" id="hppBarang" placeholder="hpp barang...">
+                </div>
+                <div class="mt-4">
+                    <label for="hargaJualBarang" class="form-label">Harga Jual Barang</label>
+                    <input type="number" class="form-control" id="hargaJualBarang" placeholder="harga jual barang...">
+                </div>
+                <button type="submit" class="mt-5 py-2 btn btn-primary w-100">Simpan</button>
+                <a href="./daftar-barang.php">
+                    <button type="button" class="mt-3 py-2 btn btn-secondary w-100">Batal</button>
+                </a>
+            </form>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- input-data-customer.php
+++ input-data-customer.php
@@ -14,7 +14,39 @@
     include_once("components/navbar.php")
     ?>
     <div class="container">
-        <h1>input data customer</h1>
+        <div class="container-form mt-4 mb-5 px-5 py-5 rounded-3">
+            <h1>Input Data Customer</h1>
+            <form action="">
+                <div class="mt-5">
+                    <label for="kodeCust" class="form-label">Kode Customer</label>
+                    <input type="text" class="form-control" id="kodeCust" placeholder="12345" disabled>
+                </div>
+                <div class="mt-4">
+                    <label for="namaCust" class="form-label">Nama Customer</label>
+                    <input type="text" class="form-control" id="namaCust" placeholder="nama kamu...">
+                </div>
+                <div class="mt-4">
+                    <label for="alamatCust" class="form-label">Alamat Customer</label>
+                    <input type="text" class="form-control" id="alamatCust" placeholder="alamat kamu...">
+                </div>
+                <div class="mt-4">
+                    <label for="telpCust" class="form-label">No Telp Customer</label>
+                    <input type="number" class="form-control" id="telpCust" placeholder="telp kamu...">
+                </div>
+                <div class="mt-4">
+                    <label for="nikCust" class="form-label">NIK Customer</label>
+                    <input type="number" class="form-control" id="nikCust" placeholder="nik kamu...">
+                </div>
+                <div class="mt-4">
+                    <label for="npwpCust" class="form-label">NPWP Customer</label>
+                    <input type="number" class="form-control" id="npwpCust" placeholder="npwp kamu...">
+                </div>
+                <button type="submit" class="mt-5 py-2 btn btn-primary w-100">Simpan</button>
+                <a href="./daftar-customer.php">
+                    <button type="button" class="mt-3 py-2 btn btn-secondary w-100">Batal</button>
+                </a>
+            </form>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
